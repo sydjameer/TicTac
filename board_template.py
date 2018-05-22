2da60f2d6cb490c7dd40b5c9bd1a084ee948e96f
sydjameer <38136854+sydjameer@users.noreply.github.com>
2018-05-22 23:35:00 +0800

    Add files via upload

---
+++ board_template.py
@@ -0,0 +1,10 @@
+def board(array):
+    print('            |      |            ')
+    print('     '+array[6]+'      |  '+array[7]+'   |     '+array[8]+'      ')
+    print('____________|______|____________')
+    print('            |      |          ')
+    print('     ' + array[3] + '      |  ' + array[4] + '   |     ' + array[5] + '      ')
+    print('____________|______|____________')
+    print('            |      |            ')
+    print('     ' + array[0] + '      |  ' + array[1] + '   |     ' + array[2] + '      ')
+    print('            |      |            ')
